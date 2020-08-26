befa602b7ba2aed20c2bebafd002a357f92cbc0f
kevindennyii <kevin.dennyii@gmail.com>
2020-08-26 01:36:36 -0500

    updates to the Recipes component implement for styling

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Welcome to Taco *Tandem*onium!
+Guac is *not* extra, cause it's on me 😉
 
 I am using a ReactJS as my framework of choice with JavaScript for this application as this is, currently, at the forefront of my mind.  I am also choosing to persist the data to local storage.  For something simple like taco recipes, I think this would be pretty efficient because my assumption is that we will not be dealing with millions of records.
 
--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Emoji from "./components/Emoji";
 //import RecipesComponent from "./components/RecipesComponent";
-import SearchRecipesComponent from "./components/SearchRecipes/SearchRecipes.component";
+import SearchRecipes from "./components/SearchRecipes/SearchRecipes.component";
+import Recipes from "./components/Recipes/Recipes.component";
 import "./App.css";
 
 //const API_KEY = "";
-
 const App = () => {
   const [recipes, setRecipes] = useState([]);
 
+  // this will grab the search value and will be used as the query on the api call
+  // instead of using the .then().then "callback hell" notation, i decided to use the
+  // async/await call as it is much more readable...readability was the main goal here.ß
   const getRecipe = async (e) => {
     const searchValue = e.target.elements.searchRecipeValue.value;
     e.preventDefault(); // need to prevent page refresh
@@ -34,33 +37,9 @@ const App = () => {
         </div>
       </header>
       <div style={{ margin: "5px" }}>
-        <SearchRecipesComponent getRecipe={getRecipe} />
-        {console.log(recipes)}
-        {recipes.map((recipe) => {
-          return (
-            <div>
-              <img
-                src={recipe.strMealThumb}
-                style={{ height: "150px", width: "150px" }}
-                alt={recipe.strMeal}
-              />
-              <p key={recipe.idMeal}>{recipe.strMeal}</p>
-            </div>
-          );
-        })}
-        {/*  <form onSubmit={handleSubmit}>*/}
-        {/*    <input*/}
-        {/*      type="text"*/}
-        {/*      name="recipe-api"*/}
-        {/*      style={{ margin: "5px" }}*/}
-        {/*      placeholder={`Enter Your Recipe API`}*/}
-        {/*      value={apiUrl}*/}
-        {/*      onChange={() => handleChange}*/}
-        {/*    />*/}
-        {/*    <input type="submit" value="submit" />*/}
-        {/*  </form>*/}
+        <SearchRecipes getRecipe={getRecipe} />
+        <Recipes recipes={recipes} />
       </div>
-      {/*<RecipesComponent apiUrl={apiUrl} />*/}
     </div>
   );
 };
